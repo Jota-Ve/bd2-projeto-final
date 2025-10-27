c52dcdf2337a72d44da701c23d4b4bf499e6bc00
João Vitor Barbosa <71910512+Jota-Ve@users.noreply.github.com>
2025-10-27 03:50:20 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,19 @@
+
+from faker import Faker
+
+import empresa_fake
+
+# Inicializa os geradores de dados em português e inglês
+faker = Faker(['pt_BR', 'en_US'])
+
+
+
 def main():
-    print("Hello from bd2-projeto-final!")
+    # Define o número de registros que deseja gerar
+    QTD_EMPRESA = 20
+    empresas = empresa_fake.EmpresaFake.gera(QTD_EMPRESA, faker=faker)
+    empresa_fake.EmpresaFake.salva_csv('dados/empresas.csv', empresas)
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
