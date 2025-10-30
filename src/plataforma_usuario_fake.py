acef3d9d51bb3d2024e94af3ac3a62d5c2aa103e
João Vitor Barbosa <71910512+Jota-Ve@users.noreply.github.com>
2025-10-30 03:56:01 -0300

    Adiciona plataforma_usuario_fake

---
+++ src/plataforma_usuario_fake.py
@@ -0,0 +1,57 @@
+"""--sql
+CREATE TABLE plataforma_usuario (
+	nro_plataforma serial4 NOT NULL,
+	nick_usuario text NOT NULL,
+	nro_usuario serial4 NOT NULL,
+	CONSTRAINT plataforma_usuario_pkey PRIMARY KEY (nro_plataforma, nick_usuario),
+	CONSTRAINT fk_plataforma_usuario_plataforma FOREIGN KEY (nro_plataforma) REFERENCES plataforma(nro) ON DELETE CASCADE ON UPDATE CASCADE,
+	CONSTRAINT fk_plataforma_usuario_usuario FOREIGN KEY (nick_usuario) REFERENCES usuario(nick) ON DELETE CASCADE ON UPDATE CASCADE
+);
+"""
+from collections.abc import Collection
+import dataclasses
+import itertools
+import random
+from typing import Any, Self, Unpack
+
+import faker as fkr
+
+from src import plataforma_fake, usuario_fake
+
+from .dado_fake import DadoFake
+
+@dataclasses.dataclass(frozen=True, slots=True, order=True)
+class PlataformaUsuarioFake(DadoFake):
+    CABECALHO = ('nro_plataforma', 'nick_usuario', 'nro_usuario')
+    nro_plataforma: int
+    nick_usuario: str
+    nro_usuario: int
+
+    type T_pk = tuple[int, str]
+    @property
+    def pk(self) -> T_pk: return (self.nro_plataforma, self.nick_usuario)
+
+    type T_dados = int
+    @property
+    def dados(self) -> T_dados: return self.nro_usuario
+
+    @property
+    def tupla(self) -> tuple[Unpack[T_pk], T_dados]: return (*self.pk, self.dados)
+
+
+    @classmethod
+    def gera(cls, quantidade: int, faker: fkr.Faker, *args: Any, plataformas: Collection[plataforma_fake.PlataformaFake],
+             usuarios: Collection[usuario_fake.UsuarioFake], **kwargs: Any) -> tuple[Self, ...]:
+
+        assert len(plataformas) * len(usuarios) >= quantidade, f"Combinações possíveis da PK abaixo da quantidade especificada: {quantidade}"
+
+        # Lista para armazenar os dados
+        plataforma_usuario: list[Self] = []
+
+        # Geração dos dados
+        plataforma_x_usuario = itertools.product(plataformas, usuarios)
+        for plataforma, usuario in random.sample(tuple(plataforma_x_usuario), quantidade):
+            # Armazena o dado gerado
+            plataforma_usuario.append(cls(plataforma.pk, usuario.pk, random.randint(0, quantidade)))
+
+        return tuple(plataforma_usuario)
