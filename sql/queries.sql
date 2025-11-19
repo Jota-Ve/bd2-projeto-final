407f1f41119123af65cec5166f446b949ec08b12
João Vitor Barbosa <71910512+Jota-Ve@users.noreply.github.com>
2025-11-19 01:13:26 -0300

    Atualizei query 3

--- sql/queries.sql
+++ sql/queries.sql
@@ -21,22 +21,33 @@ END;
 $$ LANGUAGE plpgsql;
 
 -- Consulta 3: Listar e ordenar os canais que já receberam doações e a soma dos valores recebidos em doação.
+DROP FUNCTION IF EXISTS get_channel_donation_stats(TEXT);
 CREATE OR REPLACE FUNCTION get_channel_donation_stats(channel_name TEXT DEFAULT NULL)
-RETURNS TABLE(nome_canal TEXT, nro_plataforma INT, total_doacoes NUMERIC) AS $$
+RETURNS TABLE(nro_plataforma INT, nome_canal TEXT, total_doacoes_USD NUMERIC) AS $$
 BEGIN
     RETURN QUERY
     SELECT
-        d.nome_canal,
         d.nro_plataforma,
-        SUM(d.valor) AS total_doacoes
+        d.nome_canal,
+        ROUND(SUM(d.valor * cvs.fator_conver), 2) AS total_doacoes_USD
     FROM
         doacao d
+    JOIN comentario c ON d.nro_plataforma = c.nro_plataforma
+                      AND d.nome_canal = c.nome_canal
+                      AND d.titulo_video = c.titulo_video
+                      AND d.datah_video = c.datah_video
+                      AND d.nick_usuario = c.nick_usuario
+                      AND d.seq_comentario = c.seq
+    JOIN usuario u ON c.nick_usuario = u.nick
+    JOIN pais p ON u.pais_resid = p.nome
+    join conversao cvs ON p.moeda = cvs.moeda
     WHERE
-        channel_name IS NULL OR d.nome_canal = channel_name
+        (channel_name IS NULL OR d.nome_canal = channel_name)
+        AND d.status <> 'recusado'
     GROUP BY
         d.nro_plataforma,
         d.nome_canal
     ORDER BY
-        total_doacoes DESC;
+        total_doacoes_USD DESC;
 END;
 $$ LANGUAGE plpgsql;
